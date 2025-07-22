07218a6036bb5a1399112e0e1ad59a77859d4496
Dhiraj Kapse <kapsedhiraj15@gmail.com>
2025-07-22 01:09:59 +0000

    2025-07-22 01:08:31,027: sqlalchemy.exc.ProgrammingError: (psycopg2.erro

--- Flask_Project/app/__init__.py
+++ Flask_Project/app/__init__.py
@@ -17,26 +17,9 @@ def create_app():
     bcrypt.init_app(app)
 
     # --- create tables if they don't exist ---
+    # This ensures tables are created before the app starts handling requests.
     with app.app_context():
-        # This will create any missing tables without dropping existing ones.
         db.create_all()
-
-        # Seed default departments if they are missing
-        from .models import Department
-
-        default_depts = [
-            ("AN", "Artificial Intelligence and Machine Learning"),
-            ("CE", "Civil Engineering"),
-            ("CO", "Computer Engineering"),
-            ("BD", "Cloud Computing And Big Data"),
-            ("ME", "Mechanical Engineering"),
-        ]
-
-        for code, name in default_depts:
-            if not Department.query.get(code):
-                db.session.add(Department(dept_code=code, dept_name=name))
-
-        db.session.commit()
 
     # --- register your blueprints ---
     from .routes.admin import admin_bp
