557c9c25dab1bc503a1683ab264ffb89c6c5139c
Dhiraj Kapse <kapsedhiraj15@gmail.com>
2025-07-22 02:28:58 +0000

    2025-07-22 02:27:12,655: Exception on /admin/assignments [GET] Traceback

--- Flask_Project/app/__init__.py
+++ Flask_Project/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
-from sqlalchemy import text
+from sqlalchemy import text, inspect as sqlalchemy_inspect
 
 db     = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -21,14 +21,25 @@ def create_app():
     with app.app_context():
         db.create_all()
 
-        # Add batch_number column to students table if it doesn't exist
-        # This is a simple migration to handle schema evolution without a full migration tool.
-        inspector = db.inspect(db.engine)
-        columns = [col['name'] for col in inspector.get_columns('students')]
-        if 'batch_number' not in columns:
+        inspector = sqlalchemy_inspect(db.engine)
+        
+        # --- Migrate 'students' table ---
+        student_columns = [col['name'] for col in inspector.get_columns('students')]
+        if 'batch_number' not in student_columns:
             with db.engine.connect() as connection:
                 connection.execute(text('ALTER TABLE students ADD COLUMN batch_number INTEGER'))
                 connection.commit()
+
+        # --- Migrate 'staff_subject_assignment' table ---
+        assignment_columns = [col['name'] for col in inspector.get_columns('staff_subject_assignment')]
+        with db.engine.connect() as connection:
+            if 'batch_id' not in assignment_columns:
+                connection.execute(text('ALTER TABLE staff_subject_assignment ADD COLUMN batch_id INTEGER REFERENCES batches(id)'))
+            
+            if 'classroom_id' in assignment_columns:
+                 connection.execute(text('ALTER TABLE staff_subject_assignment DROP COLUMN classroom_id'))
+            
+            connection.commit()
 
 
     # --- register your blueprints ---
